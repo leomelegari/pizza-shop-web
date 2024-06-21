fcc73ce46142aca305a8ca1caf191eb4242c869c
leomelegari <leonardo@leonardos-iMac-Pro.local>
2024-06-21 09:12:25 -0300

    basic tests on nav-link, order-status and pagination components

--- vite.config.ts
+++ vite.config.ts
@@ -15,7 +15,7 @@ export default defineConfig({
   test: {
     globals: true,
     setupFiles: ['./test/setup.ts'],
-    // environment: 'happy-dom',
+    environment: 'happy-dom',
   },
 } as UserConfig & {
   test: InlineConfig
